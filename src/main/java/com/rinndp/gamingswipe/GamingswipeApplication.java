30a7b69e475fd22a779fff9e81c6137ae52ce0ae
axel <axelperez221105@gmail.com>
2025-01-28 14:08:13 +0100

    first commit

---
+++ src/main/java/com/rinndp/gamingswipe/GamingswipeApplication.java
@@ -0,0 +1,13 @@
+package com.rinndp.gamingswipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingswipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingswipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinndp/gamingswipe/GamingswipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinndp.gamingswipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingswipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
